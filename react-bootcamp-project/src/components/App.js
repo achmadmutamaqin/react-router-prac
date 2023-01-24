7f38b16823734c13606f368a2f5302304f1663cd
Achmad Mutamaqin <achmadridzwan@gmail.com>
2023-01-24 12:37:37 +0800

    Created the routes for all the individual components

--- react-bootcamp-project/src/components/App.js
+++ react-bootcamp-project/src/components/App.js
@@ -1,24 +1,31 @@
-import logo from '../logo.svg';
+import React from 'react';
 import '../App.css';
+import Home from './Home';
+import About from './About';
+import Topics from './Topics';
+import Topic from './Topic'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ul>
+          <li align="left"><Link to='/'>Home</Link></li>
+          <li align="left"><Link to='/about'>About</Link></li>
+          <li align="left"><Link to='/topics'>Topics</Link></li>
+        </ul>
+
+        <hr />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/topics" element={<Topics />}>
+            <Route path=":topicId" element={<Topic />}></Route>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
